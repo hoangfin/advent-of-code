1831a62f0002a0516a3eff68817b4e5ee527a2e6
Hoang Tran <hoatran@student.hive.fi>
2023-12-20 18:10:38 +0200

    updated

--- 2023/17/part-one.js
+++ 2023/17/part-one.js
@@ -12,9 +12,35 @@ class City {
 	}
 
 	getNeighbors(graph) {
-
+		if (this.parent?.parent?.row === this.parent?.row && this.parent?.row === this.row) {
+			return [
+				graph[this.row - 1] ? graph[this.row - 1][this.col] : undefined,
+				graph[this.row + 1] ? graph[this.row + 1][this.col] : undefined
+			].filter(city => city !== undefined);
+		}
+		if (this.parent?.parent?.col === this.parent?.col && this.parent?.col === this.col) {
+			return [
+				graph[this.row] ? graph[this.row][this.col - 1] : undefined,
+				graph[this.row] ? graph[this.row][this.col + 1] : undefined
+			].filter(city => city !== undefined);
+		}
+		return [
+			graph[this.row - 1] ? graph[this.row - 1][this.col] : undefined,
+			graph[this.row + 1] ? graph[this.row + 1][this.col] : undefined,
+			graph[this.row] ? graph[this.row][this.col - 1] : undefined,
+			graph[this.row] ? graph[this.row][this.col + 1] : undefined
+		].filter(city => city !== undefined);
 	}
 }
+
+const buildPath = (currentCity) => {
+	const path = [];
+	while (currentCity.parent != null) {
+		path.unshift(currentCity);
+		currentCity = currentCity.parent;
+	}
+	return path;
+};
 
 const graph = Array.from(lines, (line, row) => {
 	const cities = [];
@@ -25,7 +51,6 @@ const graph = Array.from(lines, (line, row) => {
 	return cities;
 });
 
-
 const findLeastHeatLoss = (graph, startRow, startCol, endRow, endCol) => {
 
 	const calculateHeuristic = (row, col, endRow, endCol) => {
@@ -35,9 +60,6 @@ const findLeastHeatLoss = (graph, startRow, startCol, endRow, endCol) => {
 	const visited = [];
 	const unvisited = [];
 
-	const heats = Array.from(graph, () => Array.from(graph[0]).fill(Infinity));
-	const prevs = Array.from(graph, () => []);
-
 	const start = graph[startRow][startCol];
 	start.g = 0;
 	start.h = calculateHeuristic(startRow, startCol, endRow, endCol);
@@ -45,15 +67,34 @@ const findLeastHeatLoss = (graph, startRow, startCol, endRow, endCol) => {
 
 	unvisited.push(start);
 	while (unvisited.length > 0) {
-		const currentCity = unvisited.pop(); // modify later
+		const currentCity = unvisited.reduce(
+			(acc, city) => {
+				if (city && city.f < acc.f) {
+					acc = city;
+				}
+				return acc;
+			},
+			unvisited[0]
+		);
+		unvisited.splice(unvisited.indexOf(currentCity), 1);
+		if (currentCity.row === endRow && currentCity.col === endCol) {
+			const path = buildPath(currentCity);
+			const heatLoss = path.reduce((acc, city) => acc + city.heatLoss, 0);
+			console.log(path[0], "====", path.at(-1));
+			return heatLoss;
+		}
 		visited.push(currentCity);
 		for (const neighbor of currentCity.getNeighbors(graph)) {
 			if (visited.includes(neighbor))
 				continue;
 			if (!unvisited.includes(neighbor)) {
-				neighbor.parent = currentCity;
 				const tentativeG = currentCity.g + neighbor.heatLoss;
-				if (tentativeG < neighbor.g)
+				if (tentativeG < neighbor.g) {
+					neighbor.parent = currentCity;
+					neighbor.g = tentativeG;
+					neighbor.h = calculateHeuristic(neighbor.row, neighbor.col, endRow, endCol);
+					neighbor.f = neighbor.g + neighbor.h;
+				}
 				unvisited.push(neighbor);
 			}
 		}
