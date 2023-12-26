8a8e7fa5279ce1044aa8079d1bb00d7b257a0779
Hoang Tran <hoang.tran.fin@gmail.com>
2023-12-26 21:57:27 +0200

    updated

--- 2023/18/part-one.js
+++ 2023/18/part-one.js
@@ -1,33 +1,58 @@
-const { input: instructions } = require("./input");
+const { input: lines } = require("./input");
 
-const graph = Array.from({ length: instructions.length }, () => []);
+const instructions = Array.from(lines, line => line.split(" "));
 
-const fillGraph = (graph, row, col, instructions) => {
-	const instruction = instructions.shift();
-	if (!instruction) return;
-	const [direction, number] = instruction;
-	for (i = 0; i < parseInt(number); i++) {
-		
+let minX = 0;
+let minY = 0;
+
+let coordinates = instructions.reduce((acc, [dir, displacement]) => {
+	let [x, y] = acc.at(-1);
+	if (dir === 'U') {
+		y = y - parseInt(displacement);
+	} else if (dir === 'D') {
+		y = y + parseInt(displacement);
+	} else if (dir === 'R') {
+		x = x + parseInt(displacement);
+	} else if (dir === 'L') {
+		x = x - parseInt(displacement);
 	}
-	switch (direction) {
-		case 'U': {
+	if (minX > x) minX = x;
+	if (minY > y) minY = y;
+	acc.push([x, y]);
+	return acc;
+}, [[0, 0]]);
 
-		}
-		case 'R': {
+coordinates = coordinates.map(coordinate => [
+	coordinate[0] + Math.abs(minX),
+	coordinate[1] + Math.abs(minY)
+]);
 
-		}
-		case 'D': {
-
-		}
-		case 'L': {
-
-		}
-		default:
-			break;
+const fillGraph = (graph, coordinates) => {
+	if (coordinates.length <= 1) return;
+	const [x1, y1] = coordinates[0];
+	const [x2, y2] = coordinates[1];
+	console.log(x1, y1, x2, y2);
+	if (x1 !== x2) {
+		const steps = (x2 - x1) / (x2 - x1);
+		let i = x1;
+		do {
+			graph[i][y1] = '#';
+			i += steps;
+		} while (i !== x2);
 	}
+	if (y1 !== y2) {
+		const steps = (y2 - y1) / (y2 - y1);
+		let i = y1;
+		do {
+			graph[x1][i] = '#';
+			i += steps;
+		} while (i !== y2);
+	}
+	fillGraph(graph, coordinates.slice(1));
 };
 
-instructions.forEach(instruction => {
-	const [direction, number, color] = instruction.split(" ");
+const graph = Array.from({ length: 10 }, () => new Array(7).fill('.'));
 
-});
+fillGraph(graph, coordinates);
+
+graph.forEach(v => console.log(...v));
